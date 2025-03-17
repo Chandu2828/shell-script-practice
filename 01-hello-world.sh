580836a9b4cafe110d46878a905650ddeb08e3f6
Chandu2828 <purnachand7569@gmail.com>
2025-03-17 17:06:19 +0530

    hello world added

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 echo "Hello World, I am learning DevOps with Aws".
+
+echo "Chandu: Hi Sowmya, Good Morning"
+echo "Sowmya: Good Morning Sowmya"
+echo "Chandu: How are you chandu"
+echo "Sowmya: I am good, how are you chandu"
