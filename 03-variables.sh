24d9c764b2111b525bdf888193ad0b8f9b756e03
Chandu2828 <purnachand7569@gmail.com>
2025-03-17 17:47:20 +0530

    hello world added

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Hello World, I am learning DevOps with Aws".
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hi $PERSON2, Good Morning"
+echo "$PERSON2: Good Morning $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: I am good, how are you $PERSON1"
