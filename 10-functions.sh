339d5dc7dba779ac058943ba9d59fe2ba8cf2c73
Chandu2828 <purnachand7569@gmail.com>
2025-03-18 12:46:28 +0530

    added

---
+++ 10-functions.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# our program goal is to install mysql
+
+DATE=$(date +%F)
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+
+USERID=$(id -u)
+
+VALIDATE(){
+    #$! --> it will receive the argument
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2 .... FAILURE"
+        exit 1
+    else 
+        echo "$2 ... SUCCESS"
+    fi 
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: Please run this script with root access"
+    exit 1
+# else
+#     echo "INFO:: You are root user"
+fi
+
+# it is our responsibility again to check installation is success or not
+yum install mysql -y
+
+VALIDATE $? "Installing MySQL"
+
+yum install postfix -y
+
+VALIDATE $? "Installing postfix"
